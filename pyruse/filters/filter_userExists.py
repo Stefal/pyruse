14bda0a09be93d74bf334e43a7124347cf56326d
Yves G <theYinYeti@yalis.fr>
2018-01-31 13:07:05 +0100

    let the whole code enter this new year 2018 :-)

--- pyruse/filters/filter_userExists.py
+++ pyruse/filters/filter_userExists.py
@@ -1,5 +1,5 @@
 # pyruse is intended as a replacement to both fail2ban and epylog
-# Copyright © 2017 Y. Gablin
+# Copyright © 2017–2018 Y. Gablin
 # Full licensing information in the LICENSE file, or gnu.org/licences/gpl-3.0.txt if the file is missing.
 import pwd
 from pyruse import base
