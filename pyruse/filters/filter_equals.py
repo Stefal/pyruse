07ae8164cd57daf9a8185b554311e6c81c2358e7
Yves G <theYinYeti@yalis.fr>
2018-01-31 13:05:42 +0100

    fix None handling in filters

--- pyruse/filters/filter_equals.py
+++ pyruse/filters/filter_equals.py
@@ -1,5 +1,5 @@
 # pyruse is intended as a replacement to both fail2ban and epylog
-# Copyright © 2017 Y. Gablin
+# Copyright © 2017–2018 Y. Gablin
 # Full licensing information in the LICENSE file, or gnu.org/licences/gpl-3.0.txt if the file is missing.
 from pyruse import base
 
@@ -10,4 +10,4 @@ class Filter(base.Filter):
         self.value = args["value"]
 
     def filter(self, entry):
-        return entry.get(self.field, None) == self.value
+        return entry[self.field] == self.value if self.field in entry else False
